718a71f554327a9839f654d5135d3f201a519315
unknown <bhumesh.ranjaneb598@gmail.com>
2022-01-28 15:06:21 +0530

    UC1 added

---
+++ AddressBook.java
@@ -0,0 +1,69 @@
+package com.bridge.address.book.system;
+
+public class AddressBook {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private int zip;
+    private long phoneNumber;
+    private char emailID;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public char getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(char emailID) {
+        this.emailID = emailID;
+    }
+}
+
---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridge.address.book.system;
+
+public class AddressBookMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+
+    }
+}
