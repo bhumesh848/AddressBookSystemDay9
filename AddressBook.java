f6de0bb53de3e69b11cd8a9cbdbce76f912ce676
unknown <bhumesh.ranjaneb598@gmail.com>
2022-02-03 15:36:58 +0530

    UC2 added for Address Book System

--- AddressBook.java
+++ AddressBook.java
@@ -1,69 +1,23 @@
 package com.bridge.address.book.system;
 
+import java.util.ArrayList;
+
 public class AddressBook {
 
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private int zip;
-    private long phoneNumber;
-    private char emailID;
+   private ArrayList<PersonContact> contacts;
 
-    public String getFirstName() {
-        return firstName;
+    public ArrayList<PersonContact> getContacts() {
+        return contacts;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setContacts(ArrayList<PersonContact> contacts) {
+        this.contacts = contacts;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public char getEmailID() {
-        return emailID;
-    }
-
-    public void setEmailID(char emailID) {
-        this.emailID = emailID;
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contacts=" + contacts +
+                '}';
     }
 }
-
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,55 @@
 package com.bridge.address.book.system;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddressBookMain {
 
-
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        PersonContact contact = new PersonContact();
         System.out.println("Welcome to Address Book Program");
+        System.out.println("Enter first Name");
+        String firstName = sc.nextLine();
+        contact.setFirstName(firstName);
+
+        System.out.println("Enter Last Name");
+        String lastname = sc.nextLine();
+        contact.setLastName(lastname);
+
+        System.out.println("Enter the Address");
+        String address = sc.nextLine();
+        contact.setAddress(address);
+
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+        contact.setCity(city);
+
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+        contact.setState(state);
+
+        System.out.println("Enter Zip");
+        int zip = sc.nextInt();
+        contact.setZip(zip);
+
+        System.out.println("Enter Phone Number");
+        long phoneNumber = sc.nextLong();
+        contact.setPhoneNumber(phoneNumber);
+
+        System.out.println("Enter Email ID");
+        String emailID = sc.next();
+        contact.setEmailID(emailID);
+
+        System.out.println(contact);
+
+        AddressBook addressBook = new AddressBook();
+        ArrayList<PersonContact> contacts = new ArrayList<>();
+        contacts.add(contact);
+        System.out.println(Arrays.toString(contacts.toArray()));
 
     }
 }
+
